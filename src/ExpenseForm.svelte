7fc5b484b0de64f9103d3439e38397a161b13241
Suraj Pokhrel <suraj.pokhrel@sydney.edu.au>
2022-09-08 17:43:07 +1000

    added localStorage

--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -1,54 +1,73 @@
 <script>
-    import Title from "./Title.svelte";
-   export let name="";
-   export let amount;
-    export let addExpense;
-    export let isEditing;
-    export let editExpense;
-    export let handleFormClose;
+    import {onMount, onDestroy,beforeUpdate,afterUpdate} from 'svelte'
+    onMount(()=>{
+        console.log("form has mounted");
+    })
+    beforeUpdate(()=>{
+        console.log("form befor update");
+    })
+    afterUpdate(()=>{
+        console.log("form after update");
+    })
+    
+    onDestroy(()=>{
+        console.log("form is hidden");
+    })
+    
+    
 
-$: isEmpty = !name || !amount;
+  import Title from "./Title.svelte";
+  export let name = "";
+  export let amount;
+  export let addExpense;
+  export let isEditing;
+  export let editExpense;
+  export let handleFormClose;
 
-const handleSubmit=()=>{
+  $: isEmpty = !name || !amount;
+
+  const handleSubmit = () => {
     console.log("form submit");
     // console.log({name,amount});
-    if(isEditing) {
-        editExpense({name,amount});
-    }else{
-        addExpense({name,amount})
+    if (isEditing) {
+      editExpense({ name, amount });
+    } else {
+      addExpense({ name, amount });
     }
-    name="";
-    amount=null;
-}
+    name = "";
+    amount = null;
+  };
 </script>
+
 <section class="form">
+  <Title title="Add Expense" />
+  <form class="expense-form" on:submit|preventDefault={handleSubmit}>
+    <div class="form-control">
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" bind:value={name} />
+    </div>
+    <div class="form-control">
+      <label for="amout">Amount</label>
+      <input type="number" id="amout" bind:value={amount} />
+    </div>
+    {#if isEmpty}
+      <p class="form-empty">please fill out all form fields.</p>
+    {/if}
 
-    <Title title="Add Expense"/>
-    <form class="expense-form" on:submit|preventDefault= {handleSubmit}>
-        <div class="form-control">
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" bind:value={name}/>
-        </div>
-        <div class="form-control">
-            <label for="amout">Amount</label>
-            <input type="number" id="amout" bind:value={amount}/>
-        </div>
-        {#if isEmpty}
-        <p class="form-empty">please fill out all form fields.</p>
-        {/if}
-
-        <button type="submit" class="btn btn-block disabled" class:disabled={isEmpty}>
-            {#if isEditing}
-            Edit and Save
-            {:else}
-            Add Expense
-
-            {/if}
-        </button>
-        <button type="button" class="close-btn" on:click={handleFormClose}>
-            <i class="fas fa-times"/> 
-            Close
-        </button>
-    </form>
+    <button
+      type="submit"
+      class="btn btn-block disabled"
+      class:disabled={isEmpty}
+    >
+      {#if isEditing}
+        Edit and Save
+      {:else}
+        Add Expense
+      {/if}
+    </button>
+    <button type="button" class="close-btn" on:click={handleFormClose}>
+      <i class="fas fa-times" />
+      Close
+    </button>
+  </form>
 </section>
-
